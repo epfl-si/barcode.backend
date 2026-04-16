6264d59c4a22ac787e39d0a8acd83ca0198a2bd1
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-16 17:09:19 +0200

    [feature] Add `pothos` configuration

--- libs/schema.ts
+++ libs/schema.ts
@@ -1,14 +1,37 @@
-import SchemaBuilder from '@pothos/core';
+import PrismaPlugin from '@pothos/plugin-prisma';
+import SchemaBuilder from "@pothos/core";
+import PrismaTypes, {getDatamodel} from "../generated/pothos-prisma-types";
+import {prisma} from "./prisma";
 
-const builder = new SchemaBuilder({});
+const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes; // This gives the builder all the type information about your prisma schema
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+    // This give pothos information about your tables, relations, and indexes to help it generate optimal queries at runtime.
+    // This used to be attached to the prisma client, but has been removed in most runtimes/modes to reduce bundle size.
+    dmmf: getDatamodel(),
+    // warn when not using a query parameter correctly
+    onUnusedQuery: process.env.NODE_ENV === 'production' ? null : 'warn',
+  },
+});
+
+builder.prismaObject('Inventory', {
+  name: 'GetInventory',
+  fields: (t: any) => ({
+    id: t.exposeID('id_storage'),
+    barcode: t.exposeString('barcode'),
+  }),
+});
 
 builder.queryType({
   fields: (t) => ({
-    hello: t.string({
-      args: {
-        name: t.arg.string(),
+    inventoryList: t.prismaField({
+      type: 'Inventory',
+      resolve: async (query, root, args, ctx, info) => {
+        return prisma.inventory.findFirst();
       },
-      resolve: (parent, { name }) => `hello, ${name || 'World'}`,
     }),
   }),
 });
