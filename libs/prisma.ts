adb854c7c24b19282dae2129295cf5f796f51012
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-16 17:14:01 +0200

    [fix] Default port

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -7,7 +7,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: parseInt(process.env.DATABASE_PORT ?? '63306'),
+  port: parseInt(process.env.DATABASE_PORT ?? '3306'),
   connectionLimit: 5,
 });
 const prisma = new PrismaClient({
