cdc98f8ff404f2734d8063dad94675ca450b3073
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-16 20:52:39 +0200

    [refactor] Create a proper file for builder

---
+++ libs/builder.ts
@@ -0,0 +1,18 @@
+import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import PrismaTypes, {getDatamodel} from "../generated/pothos-prisma-types";
+import {prisma} from "./prisma";
+
+export const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes; // This gives the builder all the type information about your prisma schema
+}>({
+  plugins: [PrismaPlugin],
+  prisma: {
+    client: prisma,
+    // This give pothos information about your tables, relations, and indexes to help it generate optimal queries at runtime.
+    // This used to be attached to the prisma client, but has been removed in most runtimes/modes to reduce bundle size.
+    dmmf: getDatamodel(),
+    // warn when not using a query parameter correctly
+    onUnusedQuery: process.env.NODE_ENV === 'production' ? null : 'warn',
+  },
+});
--- libs/schema.ts
+++ libs/schema.ts
@@ -1,21 +1,4 @@
-import PrismaPlugin from '@pothos/plugin-prisma';
-import SchemaBuilder from "@pothos/core";
-import PrismaTypes, {getDatamodel} from "../generated/pothos-prisma-types";
-import {prisma} from "./prisma";
-
-const builder = new SchemaBuilder<{
-  PrismaTypes: PrismaTypes; // This gives the builder all the type information about your prisma schema
-}>({
-  plugins: [PrismaPlugin],
-  prisma: {
-    client: prisma,
-    // This give pothos information about your tables, relations, and indexes to help it generate optimal queries at runtime.
-    // This used to be attached to the prisma client, but has been removed in most runtimes/modes to reduce bundle size.
-    dmmf: getDatamodel(),
-    // warn when not using a query parameter correctly
-    onUnusedQuery: process.env.NODE_ENV === 'production' ? null : 'warn',
-  },
-});
+import {builder} from "./builder";
 
 builder.prismaObject('Inventory', {
   name: 'GetInventory',
