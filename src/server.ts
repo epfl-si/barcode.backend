471a7a63ecb92e35e61e93f6386e3af39415fdce
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-17 14:56:49 +0200

    [feature] Add prisma error management

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import express from "express";
 import {ApolloServer} from "@apollo/server";
 import {schema} from "../libs/prisma/schema";
 import {expressMiddleware} from "@as-integrations/express5";
+import {formatPrismaError} from "../libs/errors";
 
 export async function makeServer() {
   const app = express();
 
   const server = new ApolloServer({
-    schema
+    schema,
+    formatError(formattedError, error: any) {
+      console.error('Server error:', error, error.originalError);
+      const {errorCode, errorMessage} = formatPrismaError(formattedError, error);
+      return {extensions: {code: errorCode}, message: errorMessage};
+    }
   });
   await server.start();
 
