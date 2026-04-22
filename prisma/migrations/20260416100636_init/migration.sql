0fb7d481e3b87d37dede9498e4bb8cee6a623890
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-22 17:01:03 +0200

    [feature] Migrate DB engine toward PostgreSQL

--- prisma/migrations/20260416100636_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `inventory` (
-    `id_storage` INTEGER NOT NULL AUTO_INCREMENT,
-    `barcode` VARCHAR(80) NOT NULL,
-
-    UNIQUE INDEX `barcode`(`barcode`),
-    PRIMARY KEY (`id_storage`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/schema/prisma.ts
+++ src/schema/prisma.ts
@@ -1,15 +1,8 @@
 import "dotenv/config";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import {PrismaClient} from "../../generated/prisma/client";
+import {PrismaPg} from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma";
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: parseInt(process.env.DATABASE_PORT ?? '3306'),
-  connectionLimit: 5,
-});
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({
   adapter
 });
