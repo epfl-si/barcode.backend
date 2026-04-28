739bc94a75c6816a53b6cef58bb274d5b23ae155
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-28 17:58:16 +0200

    [feature] Add CRUD rights for queries

--- src/lib/userType.ts
+++ src/lib/userType.ts
@@ -1,7 +1,10 @@
 interface UserInfo {
-	groups: string[];
-	username: string;
-	isAdmin?: boolean;
+  groups: string[];
+  username: string;
+  isAdmin?: boolean;
   isCosec?: boolean;
   isReadOnly?: boolean;
+  canReadStorage?: boolean;
+  canCreateStorage?: boolean;
+  canDeleteStorage?: boolean;
 }
