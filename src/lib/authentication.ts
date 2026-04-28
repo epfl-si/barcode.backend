fbc9fe95bbd535989403c8a8b03eb289486950a2
Rosa Maggi <rosa.maggi@epfl.ch>
2026-04-28 17:57:32 +0200

    [fix] Spaces

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -4,83 +4,83 @@ import jwt from 'jsonwebtoken'
 import jwksClient from 'jwks-rsa';
 
 export function getBearerToken(req: Request<{}, any, any, ParsedQs, Record<string, any>>): string | undefined {
-	const matched = req.headers.authorization?.match(/^Bearer\s(.*)$/);
-	if (!matched) return undefined;
-	else return matched[1];
+  const matched = req.headers.authorization?.match(/^Bearer\s(.*)$/);
+  if (!matched) return undefined;
+  else return matched[1];
 }
 
 export async function authenticateFromBearerToken(req: Request<{}, any, any, ParsedQs, Record<string, any>>): Promise<UserInfo> {
-	const access_token = getBearerToken(req);
-	if (!access_token) {
-		throw new Error("Unauthorized");
-	}
+  const access_token = getBearerToken(req);
+  if (!access_token) {
+    throw new Error("Unauthorized");
+  }
 
-	await checkTokenValid(access_token);
+  await checkTokenValid(access_token);
 
-	const authenticationResult = parseJwt(access_token);
+  const authenticationResult = parseJwt(access_token);
 
-	const userGroups = authenticationResult.groups;
-	const allowedGroups = (process.env.ALLOWED_GROUPS ?? '').split(',');
-	if (!(userGroups && userGroups.some((e: string) => allowedGroups.includes(e)))) {
-		throw new Error("Unauthorized");
-	}
+  const userGroups = authenticationResult.groups;
+  const allowedGroups = (process.env.ALLOWED_GROUPS ?? '').split(',');
+  if (!(userGroups && userGroups.some((e: string) => allowedGroups.includes(e)))) {
+    throw new Error("Unauthorized");
+  }
 
-	const user: UserInfo = {
-		groups: userGroups,
-		username: ( authenticationResult.unique_name ||
-			authenticationResult.gaspar )  // EPFL-ism in Entra
-	};
+  const user: UserInfo = {
+    groups: userGroups,
+    username: ( authenticationResult.unique_name ||
+      authenticationResult.gaspar )  // EPFL-ism in Entra
+  };
 
-	const hasRoleAdmin = user.groups.indexOf((process.env.ADMIN_GROUP ?? '')) > -1;
-	const hasRoleCosec = user.groups.indexOf((process.env.COSEC_GROUP ?? '')) > -1;
+  const hasRoleAdmin = user.groups.indexOf((process.env.ADMIN_GROUP ?? '')) > -1;
+  const hasRoleCosec = user.groups.indexOf((process.env.COSEC_GROUP ?? '')) > -1;
   const hasRoleReadOnly = user.groups.indexOf((process.env.RO_GROUP ?? '')) > -1;
 
-	user.isAdmin = hasRoleAdmin;
-	user.isCosec = hasRoleCosec;
+  user.isAdmin = hasRoleAdmin;
+  user.isCosec = hasRoleCosec;
   user.isReadOnly = hasRoleReadOnly;
 
-	return user;
+  return user;
 }
 
 function parseJwt (access_token: string) {
-	return JSON.parse(Buffer.from(access_token.split('.')[1], 'base64').toString());
+  return JSON.parse(Buffer.from(access_token.split('.')[1], 'base64').toString());
 }
 
 const getJwksUri = (() => {
-	let cachedJwksUri: string;
-	return async function getJwksUri () {
-		if (! cachedJwksUri) {
-			if (process.env.OIDC_KEYS_URL) {
-				cachedJwksUri = process.env.OIDC_KEYS_URL;
-			} else {
-				const response = await fetch(`${process.env.OIDC_BASE_URL}/.well-known/openid-configuration`);
-				const wellKnown = await response.json();
-				cachedJwksUri = wellKnown.jwks_uri;
-			}
-		}
-		return cachedJwksUri;
-	 }
+  let cachedJwksUri: string;
+  return async function getJwksUri () {
+    if (! cachedJwksUri) {
+      if (process.env.OIDC_KEYS_URL) {
+        cachedJwksUri = process.env.OIDC_KEYS_URL;
+      } else {
+        const response = await fetch(`${process.env.OIDC_BASE_URL}/.well-known/openid-configuration`);
+        const wellKnown = await response.json();
+        cachedJwksUri = wellKnown.jwks_uri;
+      }
+    }
+    return cachedJwksUri;
+   }
 })();
 
 async function checkTokenValid(access_token: string) {
-	const client = jwksClient({ jwksUri: await getJwksUri() });
+  const client = jwksClient({ jwksUri: await getJwksUri() });
 
-	const getKey = (header: any, callback: any) => {
-		client.getSigningKey(header.kid, (err, key) => {
-			if (err) return callback(err);
-			const signingKey = key?.getPublicKey();
-			callback(null, signingKey);
-		});
-	}
+  const getKey = (header: any, callback: any) => {
+    client.getSigningKey(header.kid, (err, key) => {
+      if (err) return callback(err);
+      const signingKey = key?.getPublicKey();
+      callback(null, signingKey);
+    });
+  }
 
-	return new Promise((resolve,reject) =>
-		jwt.verify(access_token, getKey, {
-			algorithms: ['RS256'],
-			issuer: process.env.OIDC_BASE_URL,
-			audience: process.env.OIDC_CLIENT_ID
-		},
-		function(err,decoded) {
-			return err ? reject(err) : resolve(decoded);
-		}
-	));
+  return new Promise((resolve,reject) =>
+    jwt.verify(access_token, getKey, {
+      algorithms: ['RS256'],
+      issuer: process.env.OIDC_BASE_URL,
+      audience: process.env.OIDC_CLIENT_ID
+    },
+    function(err,decoded) {
+      return err ? reject(err) : resolve(decoded);
+    }
+  ));
 }
