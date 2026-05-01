07144d854260ed11dd282b25b72feaca48efb72c
Rosa Maggi <rosa.maggi@epfl.ch>
2026-05-01 17:07:01 +0200

    [fix] Indent

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,11 +1,11 @@
 const wellKnowsErrors: Record<string, string> = {
-	GRAPHQL_PARSE_FAILED:         "The operation string contains a syntax error",
-	GRAPHQL_VALIDATION_FAILED:    "The operation is not valid against the server’s schema",
-	BAD_USER_INPUT:               "The operation includes an invalid value for a field argument",
-	BAD_REQUEST:                  "The operation includes invalid variables",
-	P2003:                        'Is not possible to perform this action because of some relationship',
-	P2002:                        'An element with this name appears to already exist',
-	FNP:                          'Filename not permitted'
+  GRAPHQL_PARSE_FAILED:         "The operation string contains a syntax error",
+  GRAPHQL_VALIDATION_FAILED:    "The operation is not valid against the server’s schema",
+  BAD_USER_INPUT:               "The operation includes an invalid value for a field argument",
+  BAD_REQUEST:                  "The operation includes invalid variables",
+  P2003:                        'Is not possible to perform this action because of some relationship',
+  P2002:                        'An element with this name appears to already exist',
+  FNP:                          'Filename not permitted'
 }
 
 /**
@@ -17,5 +17,5 @@ export function formatPrismaError(formattedError: any, error: { originalError: {
   const errorCode: string = (error?.originalError?.code || formattedError?.extensions?.code || formattedError?.code) as string;
   const errorMessage = errorCode in wellKnowsErrors ? wellKnowsErrors[errorCode] : (error.message || 'Internal Server Error');
   const httpCode = error.httpCode ?? (errorMessage === 'Unauthorized' ? 403 : 500);
-	return {errorCode, errorMessage};
+  return {errorCode, errorMessage};
 }
