5f9420fba9d6a026edadf1be6b0142a9db9733fa
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-12 17:13:53 +0200

    [feature] Add rmm status

--- prisma/migrations/20260603093939_add_status_for_rmm/migration.sql
+++ prisma/migrations/20260603093939_add_status_for_rmm/migration.sql
@@ -1,19 +1,19 @@
 /*
   Warnings:
 
-  - Added the required column `rmmStatus` to the `boxes` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `rmmStatus` to the `shelves` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `rmmStatus` to the `storages` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `rmm_status` to the `boxes` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `rmm_status` to the `shelves` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `rmm_status` to the `storages` table without a default value. This is not possible if the table is not empty.
 
 */
 -- CreateEnum
 CREATE TYPE "rmm_code_status" AS ENUM ('Created', 'Deleted', 'DeleteNotifSent', 'RestoreNotifSent', 'ToBeCreated', 'ToBeDeleted');
 
 -- AlterTable
-ALTER TABLE "boxes" ADD COLUMN     "rmmStatus" "rmm_code_status" NOT NULL;
+ALTER TABLE "boxes" ADD COLUMN     "rmm_status" "rmm_code_status" NOT NULL;
 
 -- AlterTable
-ALTER TABLE "shelves" ADD COLUMN     "rmmStatus" "rmm_code_status" NOT NULL;
+ALTER TABLE "shelves" ADD COLUMN     "rmm_status" "rmm_code_status" NOT NULL;
 
 -- AlterTable
-ALTER TABLE "storages" ADD COLUMN     "rmmStatus" "rmm_code_status" NOT NULL;
+ALTER TABLE "storages" ADD COLUMN     "rmm_status" "rmm_code_status" NOT NULL;
