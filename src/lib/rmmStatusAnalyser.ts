87f09b96f48704f2bb913b90bd42df182e477c4e
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-16 23:47:05 +0200

    [feature] Check RMM barcode status

---
+++ src/lib/rmmStatusAnalyser.ts
@@ -0,0 +1,19 @@
+import {getContainerFromRMM} from "./api";
+
+export async function callRmmAndGetStatusForDeletion (ctx: any, codes: {barcode: string}[]) {
+  // Check RMM if barcode is empty and its children too, make transaction, otherwise throw error
+  const rmmResult = await getContainerFromRMM({barcodes: codes.map(c => c.barcode).join(',')});
+  const codeResult = codes.map(c => {
+    const rmmCode = rmmResult.rows.find((r: { barcode: string; }) => r.barcode === c.barcode);
+    let status = '';
+    if (!rmmCode || rmmCode.status === 'INACTIF') {
+      status = "Deleted"; // barcodes are not presents on RMM or it's inactive
+    } else if (rmmCode.status === 'AVAILABLE' ) { //&& empty
+      status =  "ToBeDeleted"; // the barcode is present on RMM, and it's active and empty
+    } else {
+      throw new Error(`The code ${rmmCode.barcode} is not empty. It can't be deleted.`);
+    }
+    return {...c, rmmStatus: status};
+  });
+  return codeResult.filter(cr => cr.rmmStatus === 'ToBeDeleted').length > 0 ? 'ToBeDeleted' : 'Deleted';
+}
