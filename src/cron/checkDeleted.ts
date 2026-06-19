c22a88dd650fd0b6480a74f1d36ddc11943a80e4
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-19 11:55:18 +0200

    [refactor] Create generic method for setting RMM status to location

--- src/cron/checkDeleted.ts
+++ src/cron/checkDeleted.ts
@@ -1,6 +1,5 @@
 import {getPrismaForUser} from "../lib/auditablePrisma";
-import {getLocationsByRMMStatus, setLocationsRMMCode} from "../schema/types/location";
-import {callRmmAndGetAllDeleted} from "../lib/rmmStatusAnalyser";
+import {setLocationStatusByRMM} from "../lib/rmmStatusAnalyser";
 
 const cronUser: UserInfo = {
   username: 'LHD-cron'
@@ -12,41 +11,4 @@ const prisma = getPrismaForUser(cronUser);
  *
  * For each code, update status if it has been deleted on RMM
  */
-async function checkForDeleteNotifSentCodes () {
-  const storages =  await getLocationsByRMMStatus(prisma, 'storage', 'DeleteNotifSent');
-  const shelves =  await getLocationsByRMMStatus(prisma, 'shelf', 'DeleteNotifSent');
-  const boxes =  await getLocationsByRMMStatus(prisma, 'box', 'DeleteNotifSent');
-
-  const codes = [
-    ...storages.map((code: { barcode: string; }) => {
-      return {barcode: code.barcode, locationName: 'storage'}
-    }),
-    ...shelves.map((code: { barcode: string; }) => {
-      return {barcode: code.barcode, locationName: 'shelf'}
-    }),
-    ...boxes.map((code: { barcode: string; }) => {
-      return {barcode: code.barcode, locationName: 'box'}
-    })
-  ]
-  const deletedCodes = await callRmmAndGetAllDeleted(codes);
-  console.log(`Delete codes: ${deletedCodes.map(c => c.barcode).join('\n')}`);
-  await prisma.$transaction(async (tx) => {
-    await setLocationsRMMCode(tx,
-      'storage',
-      deletedCodes.filter(code => code.locationName === 'storage').map(c => c.barcode),
-      'Deleted');
-    await setLocationsRMMCode(tx,
-      'shelf',
-      deletedCodes.filter(code => code.locationName === 'shelf').map(c => c.barcode),
-      'Deleted');
-    await setLocationsRMMCode(tx,
-      'box',
-      deletedCodes.filter(code => code.locationName === 'box').map(c => c.barcode),
-      'Deleted');
-  },{
-    maxWait: 10000, // Max time (ms) to wait for a transaction slot (default: 2000)
-    timeout: 30000, // Max time (ms) the transaction can run (default: 5000)
-  });
-}
-
-checkForDeleteNotifSentCodes();
+await setLocationStatusByRMM(prisma, "DeleteNotifSent", "Deleted");
--- src/lib/rmmStatusAnalyser.ts
+++ src/lib/rmmStatusAnalyser.ts
@@ -1,4 +1,6 @@
 import {getContainerFromRMM} from "./api";
+import {getLocationsByRMMStatus, setLocationsRMMCode} from "../schema/types/location";
+import {RMMCodeStatus} from '../../generated/prisma';
 
 export async function callRmmAndGetStatusForDeletion (codes: {barcode: string}[]) {
   // Check RMM if barcode is empty and its children too, make transaction, otherwise throw error
@@ -18,15 +20,54 @@ export async function callRmmAndGetStatusForDeletion (codes: {barcode: string}[]
   return codeResult.filter(cr => cr.rmmStatus === 'ToBeDeleted').length > 0 ? 'ToBeDeleted' : 'Deleted';
 }
 
-export async function callRmmAndGetAllDeleted (codes: {barcode: string, locationName: 'storage' | 'shelf' | 'box'}[]) {
+export async function getCodesByStatusFromRMM (codes: {barcode: string, locationName: 'storage' | 'shelf' | 'box'}[], filter: RMMCodeStatus) {
   const rmmResult = await getContainerFromRMM({barcodes: codes.map(c => c.barcode).join(',')});
   const codeResult = codes.map(c => {
     const rmmCode = rmmResult.rows.find((r: { barcode: string; }) => r.barcode === c.barcode);
     let status = '';
     if (!rmmCode || rmmCode.status === 'INACTIF') {
-      status = "Deleted"; // barcodes are not presents on RMM or it's inactive
+      status = "Deleted"; // barcode is not present on RMM, or it's inactive;
+    } else if (rmmCode && rmmCode.status === 'AVAILABLE') {
+      status = "Created"; // barcode is presents on RMM and it's active
     }
     return {...c, rmmStatus: status};
   });
-  return codeResult.filter(cr => cr.rmmStatus === 'Deleted');
+  return codeResult.filter(cr => cr.rmmStatus === filter);
+}
+
+export async function setLocationStatusByRMM (prisma: any, oldRMMStatus: RMMCodeStatus, newRMMStatus: RMMCodeStatus) {
+  const storages =  await getLocationsByRMMStatus(prisma, 'storage', oldRMMStatus);
+  const shelves =  await getLocationsByRMMStatus(prisma, 'shelf', oldRMMStatus);
+  const boxes =  await getLocationsByRMMStatus(prisma, 'box', oldRMMStatus);
+
+  const codes = [
+    ...storages.map((code: { barcode: string; }) => {
+      return {barcode: code.barcode, locationName: 'storage'}
+    }),
+    ...shelves.map((code: { barcode: string; }) => {
+      return {barcode: code.barcode, locationName: 'shelf'}
+    }),
+    ...boxes.map((code: { barcode: string; }) => {
+      return {barcode: code.barcode, locationName: 'box'}
+    })
+  ]
+  const restoredCodes = await getCodesByStatusFromRMM(codes, newRMMStatus);
+  console.log(`Create codes: ${restoredCodes.map(c => c.barcode).join('\n')}`);
+  await prisma.$transaction(async (tx: any) => {
+    await setLocationsRMMCode(tx,
+      'storage',
+      restoredCodes.filter(code => code.locationName === 'storage').map(c => c.barcode),
+      newRMMStatus);
+    await setLocationsRMMCode(tx,
+      'shelf',
+      restoredCodes.filter(code => code.locationName === 'shelf').map(c => c.barcode),
+      newRMMStatus);
+    await setLocationsRMMCode(tx,
+      'box',
+      restoredCodes.filter(code => code.locationName === 'box').map(c => c.barcode),
+      newRMMStatus);
+  },{
+    maxWait: 10000, // Max time (ms) to wait for a transaction slot (default: 2000)
+    timeout: 30000, // Max time (ms) the transaction can run (default: 5000)
+  });
 }
