a91c0e12ad48a9f7f749eab4a9ec09c38fa599b2
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-22 09:49:17 +0200

    [feature] Cronjob for deletion and creation

---
+++ src/cron/notifyDeletion.ts
@@ -0,0 +1,65 @@
+import {getPrismaForUser} from "../lib/auditablePrisma";
+import {sendEmailForToBeDeletedCodes} from "../lib/email/mailer";
+import {getFormattedDate} from "../lib/date";
+import {getCodesByStatus, setLocationsRMMCode} from "../schema/types/location";
+import {getContainerFromRMM, getRoomFromApiByName} from "../lib/api";
+
+const cronUser: UserInfo = {
+  username: 'LHD-cron'
+};
+const prisma = getPrismaForUser(cronUser);
+
+/**
+ * Check for all ToBeDeleted codes
+ *
+ * For each code, notify Catalyse
+ */
+async function notifyForToBeDeletedCodes () {
+  // Get details for each code from DB given its RMM status
+  const codes: {barcode: string, locationName: "storage" | "shelf" | "box", deletedOn: Date, deletedBy: string, parentNiv1: string, parentNiv2: string, parentNiv3: string}[] = await getCodesByStatus(prisma, 'ToBeDeleted');
+  const containers = [];
+  for ( const code of codes) {
+    // Get roomName by location type
+    let roomName = '';
+    if (code.locationName === 'storage') {
+      roomName = code.parentNiv1;
+    } else if (code.locationName === 'shelf') {
+      roomName = code.parentNiv2;
+    } else if (code.locationName === 'box') {
+      roomName = code.parentNiv3;
+    }
+    // Call api to get Site>Building>Floor given the room
+    const room: { name: string; building: string; site: string; floor: string; } = await getRoomFromApiByName(roomName);
+    // Build querystring
+    let location = '';
+    if (code.locationName === 'storage') {
+      location = `${room.site}>${room.building}>${room.floor}>${roomName}>${code.barcode}`;
+    } else if (code.locationName === 'shelf') {
+      location = `${room.site}>${room.building}>${room.floor}>${roomName}>${code.parentNiv1}>${code.barcode}`;
+    } else if (code.locationName === 'box') {
+      location = `${room.site}>${room.building}>${room.floor}>${roomName}>${code.parentNiv2}>${code.parentNiv1}>${code.barcode}`;
+    }
+    // Check RMM if barcode sublocation contains something
+    const availableRMMContainers = await getContainerFromRMM({locations: location, status: 5});
+    // SubLocation could be deleted only if totalcount === 0
+    containers.push({...code, totalCount: availableRMMContainers.totalCount});
+  }
+  const message: string[] = containers.map((code: {barcode: string, locationName: "storage" | "shelf" | "box", deletedOn: Date, deletedBy: string, totalCount: number}) =>
+    `${code.barcode} - Supprimé le ${getFormattedDate(code.deletedOn)} par ${code.deletedBy}. Il contient ${code.totalCount} items.`);
+
+  console.log(`Sending notification for ToBeDeleted codes: ${message.join('\n')}`);
+  await sendEmailForToBeDeletedCodes(message.join('<br/>'));
+  await prisma.$transaction(async (tx) => {
+    await setLocationsRMMCode(tx, 'storage', codes.filter(c => c.locationName === 'storage')
+      .map(c => c.barcode), 'DeleteNotifSent');
+    await setLocationsRMMCode(tx, 'shelf', codes.filter(c => c.locationName === 'shelf')
+      .map(c => c.barcode), 'DeleteNotifSent');
+    await setLocationsRMMCode(tx, 'box', codes.filter(c => c.locationName === 'box')
+      .map(c => c.barcode), 'DeleteNotifSent');
+  },{
+    maxWait: 10000, // Max time (ms) to wait for a transaction slot (default: 2000)
+    timeout: 30000, // Max time (ms) the transaction can run (default: 5000)
+  });
+}
+
+notifyForToBeDeletedCodes();
--- src/schema/types/location.ts
+++ src/schema/types/location.ts
@@ -1,4 +1,8 @@
 import {RMMCodeStatus} from '../../../generated/prisma';
+import {getBoxesByRMMStatus} from "./box";
+import {getShelvesByRMMStatus} from "./shelf";
+import {getStoragesByRMMStatus} from "./storage";
+
 
 export async function restoreLocation (transaction: any, locationName: 'storage' | 'shelf' | 'box', barcode: string) {
   await transaction[locationName].update({
@@ -22,4 +26,46 @@ export async function setLocationsRMMCode (transaction: any, locationName: 'stor
       rmmStatus: status
     }
   });
+}
+
+export async function getCodesByStatus (prisma: any, status: RMMCodeStatus) {
+  const storages =  await getStoragesByRMMStatus(prisma, status);
+  const shelves =  await getShelvesByRMMStatus(prisma,  status);
+  const boxes =  await getBoxesByRMMStatus(prisma, status);
+
+  return [
+    ...storages.map((code: { barcode: any; deletedBy: any; deletedOn: any; roomDisplay: any; }) => {
+      return {
+        barcode: code.barcode,
+        locationName: 'storage',
+        deletedBy: code.deletedBy,
+        deletedOn: code.deletedOn,
+        parentNiv1: code.roomDisplay,
+        parentNiv2: null,
+        parentNiv3: null
+      }
+    }),
+    ...shelves.map((code: { barcode: any; deletedBy: any; deletedOn: any; storage: { barcode: any; roomDisplay: any; }; }) => {
+      return {
+        barcode: code.barcode,
+        locationName: 'shelf',
+        deletedBy: code.deletedBy,
+        deletedOn: code.deletedOn,
+        parentNiv1: code.storage.barcode,
+        parentNiv2: code.storage.roomDisplay,
+        parentNiv3: null
+      }
+    }),
+    ...boxes.map((code: { barcode: any; deletedBy: any; deletedOn: any; shelf: { barcode: any; storage: { barcode: any; roomDisplay: any; }; }; }) => {
+      return {
+        barcode: code.barcode,
+        locationName: 'box',
+        deletedBy: code.deletedBy,
+        deletedOn: code.deletedOn,
+        parentNiv1: code.shelf.barcode,
+        parentNiv2: code.shelf.storage.barcode,
+        parentNiv3: code.shelf.storage.roomDisplay
+      }
+    })
+  ];
 }
