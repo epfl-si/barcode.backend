c9ae0c0341bca853e443c2dffb6810044de5c4be
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-26 12:21:37 +0200

    [fix] field in RMM API result

--- src/cron/notifyDeletion.ts
+++ src/cron/notifyDeletion.ts
@@ -43,7 +43,7 @@ async function notifyForToBeDeletedCodes () {
     try {
       const availableRMMContainers = await callRMM('/epfl/erd-services/json/containersearch/search', {locations: location, status: 5, timezoneoffset: 0});
       // SubLocation could be deleted only if totalcount === 0
-      containers.push({...code, totalCount: availableRMMContainers.totalCount});
+      containers.push({...code, totalCount: availableRMMContainers.totalResults});
     } catch ( e ) {
       await prisma.$transaction(async (tx) => {
         await setLocationsRMMCode(tx, code.locationName, [code].map(c => c.barcode), 'Deleted');
