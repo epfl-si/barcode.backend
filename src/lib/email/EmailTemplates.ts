ad200dcaf75c7c5a35504e7cc217cb2bd950b31b
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-30 16:47:57 +0200

    [feature] Send Email for NotAllowed rooms in RMM

--- src/lib/email/EmailTemplates.ts
+++ src/lib/email/EmailTemplates.ts
@@ -13,6 +13,16 @@ Merci,<br/><br/>
 Le service LIL`,
 }
 
+export const notAllowedRooms: EmailTemplate = {
+  subject: `LIL - Liste des codes non créés dans RMM`, // TODO email Snow
+  body: `Bonjour,<br/><br/>
+Voici la liste des codes qui n'ont pas été créés correctement dans RMM et <b>qui doivent être traité manuellement</b>: <br/><br/>
+{{message}}<br/><br/>
+Pour toutes questions, merci de contacter le support Catalyse.<br/><br/>
+Merci,<br/><br/>
+Le service LIL`,
+}
+
 export function logRecipients (to: string[], cc: string[], bcc: string[]) {
 	return `<b>TO</b>: ${to.join(', ')}<br/>
 	<b>CC</b>: ${cc.join(', ')}<br/>
