d97116356a578541b26dc373d428421e91748996
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-30 16:46:36 +0200

    [fix] field name

--- src/schema/types/shelf.ts
+++ src/schema/types/shelf.ts
@@ -13,7 +13,7 @@ builder.prismaObject('Shelf', {
     deletedBy: t.exposeString('deletedBy'),
     deletedOn: t.expose('deletedOn', { type: 'DateTime', nullable: true }),
     rmmStatus: t.exposeString('rmmStatus'),
-    rmmMessage: t.exposeString('rmmStatus'),
+    rmmMessage: t.exposeString('rmmMessage'),
     boxes: t.relation('boxes', {
       authScopes: {
         needPermission: 'canReadBox'
