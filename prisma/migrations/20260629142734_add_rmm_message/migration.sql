3a9c1dfe17304ab12eedd064d9940b037569466d
Rosa Maggi <rosa.maggi@epfl.ch>
2026-06-30 15:07:14 +0200

    [feature] Add RMM error message

---
+++ prisma/migrations/20260629142734_add_rmm_message/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "boxes" ADD COLUMN     "rmm_message" TEXT;
+
+-- AlterTable
+ALTER TABLE "shelves" ADD COLUMN     "rmm_message" TEXT;
+
+-- AlterTable
+ALTER TABLE "storages" ADD COLUMN     "rmm_message" TEXT;
