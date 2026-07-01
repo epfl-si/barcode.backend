211e8fc44aa23ac17a283db30b6be1b09d4641bc
Rosa Maggi <rosa.maggi@epfl.ch>
2026-07-01 11:03:56 +0200

    [fix] Email text

--- src/lib/email/EmailTemplates.ts
+++ src/lib/email/EmailTemplates.ts
@@ -6,7 +6,7 @@ export type EmailTemplate = {
 export const toBeDeletedCodes: EmailTemplate = {
 	subject: `LIL - Liste des codes supprimés`,
 	body: `Bonjour,<br/><br/>
-Voici la liste des codes qui ont été supprimés dans LIL et <b>qui doivent être supprimés dans RMM</b>: <br/><br/>
+Voici la liste des location(s) qui ont été supprimés dans LIL et <b>qui doivent être supprimés dans RMM</b>: <br/><br/>
 {{message}}<br/><br/>
 Pour toutes questions, merci de contacter le support Catalyse.<br/><br/>
 Merci,<br/><br/>
@@ -16,7 +16,7 @@ Le service LIL`,
 export const notAllowedRooms: EmailTemplate = {
   subject: `LIL - Liste des codes non créés dans RMM`, // TODO email Snow
   body: `Bonjour,<br/><br/>
-Voici la liste des codes qui n'ont pas été créés correctement dans RMM et <b>qui doivent être traité manuellement</b>: <br/><br/>
+Voici la liste des location(s) qui n'ont pas été créés correctement dans RMM et <b>qui doivent être traité manuellement</b>: <br/><br/>
 {{message}}<br/><br/>
 Pour toutes questions, merci de contacter le support Catalyse.<br/><br/>
 Merci,<br/><br/>
