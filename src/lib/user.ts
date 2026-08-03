6e5b8e7771081c51c23f713fae0ac02e3112be7a
Rosa Maggi <rosa.maggi@epfl.ch>
2026-08-03 14:07:06 +0200

    [feature] Extract sciper from user

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,3 +1,14 @@
 export function getUserString (user: UserInfo) {
   return `${user.name}${user.sciper ? ` (${user.sciper})` : ''}`;
 }
+
+export function extractSciper(user: string) {
+  const start = user.indexOf("(");
+  const end = user.indexOf(")");
+
+  if (start === -1 || end === -1 || end < start) {
+    return null;
+  }
+
+  return user.substring(start + 1, end);
+}
